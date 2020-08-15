f910a4af9ff67c9055b6e9fdf7192a646d874386
freedog1 <koki375656@gmail.com>
2020-08-15 17:21:41 +0900

    basic timer db

---
+++ record.php
@@ -0,0 +1,62 @@
+<?php
+//DB接続
+try {
+    // PDOインスタンスを生成    
+    $ini = parse_ini_file('./db.ini',FALSE);
+    $pdo = new PDO('mysql:host='.$ini['host'].';dbname='.$ini['dbname'].';charset=utf8', $ini['dbuser'], $ini['dbpass']);
+    
+        // エラー（例外）が発生した時の処理を記述
+        } catch (PDOException $e) {
+
+          // エラーメッセージを表示させる
+          echo 'データベースにアクセスできません！' . $e->getMessage();
+          // 強制終了
+          exit;
+        }
+
+
+function record(){
+                    $sql = "INSERT INTO users (start_at,stop_at,time,stop_count) VALUES  (:start_at,:stop_at,:time,:stop_count)";
+//                    $sql = "INSERT INTO work (work_time) VALUES  (:work_time)";
+                    $stmt = $pdo->prepare($sql);
+                    date_default_timezone_set('Asia/Tokyo');
+                    $stmt->bindValue(':start_at',date("Y/m/d H:i:s"));
+                    $stmt->bindValue(':stop_at',date("Y/m/d H:i:s"));
+                    $stmt->bindValue(':stop_count',25);
+                    $stmt->bindValue(':work_time',25);
+                    if($stmt->execute()){
+                        echo "成功";
+                    }else{
+                        echo "失敗";
+    
+                    }
+}
+
+//ajax送信でPOSTされたデータを受け取る
+$work_time = $_POST['work_time'];
+$post_data_2 = $_POST['post_data_2'];
+//受け取ったデータを配列に格納
+$return_array = array($work_time, $post_data_2);
+
+                    $sql = "INSERT INTO work (start_at,work_time,stop_count) VALUES (:start_at,:work_time,:stop_count)";
+
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->bindValue(':start_at',date("Y/m/d H:i:s"));
+                    
+                    $stmt->bindValue(':stop_count',25);
+                    $stmt->bindValue(':work_time',25);
+                    echo date("Y/m/d H:i:s");
+                   if($stmt->execute()){
+                        echo "成功";
+                    }else{
+                        echo "失敗";
+    
+                    }
+
+//ヘッダーの設定
+header('Content-type:application/json; charset=utf8');
+//「$return_array」をjson_encodeして出力
+echo json_encode($return_array);
+
+
+?>
