71893ebaf792f50f5bc148220083f719fac3cedc
freedog1 <koki375656@gmail.com>
2020-09-07 00:22:47 +0900

    bootstrap適用

--- callback.php
+++ callback.php
@@ -1,6 +1,6 @@
 <?php
   require_once("env.php"); // 環境設定ファイルの読み込み
-  require("show_status.php");
+  
   // アクセストークンを取得する
   // POSTヘッダを生成する
   $header = [
@@ -38,33 +38,152 @@
   $access_token = $token['access_token']; // アクセストークン
   // $user_id = $token['user_id']; // ユーザID
 
+  function todayWorkTime($i){
+        try {
+            // PDOインスタンスを生成    
+            $ini = parse_ini_file('./db.ini',FALSE);
+            $pdo = new PDO('mysql:host='.$ini['host'].';dbname='.$ini['dbname'].';charset=utf8', $ini['dbuser'], $ini['dbpass']);
+
+                // エラー（例外）が発生した時の処理を記述
+                } catch (PDOException $e) {
+
+                  // エラーメッセージを表示させる
+                  echo 'データベースにアクセスできません！' . $e->getMessage();
+                  // 強制終了
+                  exit;
+                }
+
+            $daysql = "SELECT * FROM work WHERE stop_at BETWEEN :today1 AND :today2";
+            $weeksql = "SELECT * FROM work WHERE stop_at BETWEEN :week1 AND :today2";
+            $day = $pdo->prepare($daysql);
+            $week = $pdo->prepare($weeksql);
+
+            date_default_timezone_set('Asia/Tokyo');
+            $day->bindValue(':today1',date("Y/m/d 00:00:00"));
+            $day->bindValue(':today2',date("Y/m/d 23:59:59"));
+            $week->bindValue(':week1',date("Y/m/d 00:00:00", strtotime("-6 day")));
+            $week->bindValue(':today2',date("Y/m/d 23:59:59"));
+            if($day->execute()){
+    //          テーブルのレコード数を取得する
+                $day_row_cnt = $day->rowCount();
+                if($i == 0){
+                    $todayTime = $day_row_cnt * 0.42;
+                    echo $todayTime;
+                    echo "h";
+                }elseif($i == 1){
+                    echo $day_row_cnt;
+                    echo "回";
+                }
+            }
+            if($week->execute()){
+    //        テーブルのレコード数を取得する
+                $week_row_cnt = $week->rowCount();
+                if($i == 2){
+                    $weekTime = $week_row_cnt * 0.42;
+                    echo $weekTime;
+                    echo "h";
+                }elseif($i == 3){
+                    echo $week_row_cnt;
+                    echo "回";
+                }
+
+            }
+    }
+
 
 ?>
-
-  
-
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="ja" >
+  <meta charset="utf-8">
   <head>
-    <meta charset="utf-8">
-<!--        日付取得のライブラリ-->
-        <link rel="stylesheet" href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
-        <script src="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
-        <!-- jquery-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <title>Fitbit HeartRate PHP7.0</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>ポモドーロ</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+    <link href="cover.css" rel="stylesheet">
+    <!--  日付取得のライブラリ-->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
+    <script src="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
+
+    
+        <style>
+        #rest-time{
+            display: none;
+        }
+        #timerType{
+
+    color: darkslateblue;
+    background: #c6e4ff;
+    border-bottom: solid 6px #aac5de;
+    border-radius: 9px;
+            font-size: 40px;
+        }
+        .card-deck{
+            font-size: 30px;
+            color:black;
+            
+          }
+          .font-weight-normal{
+            font-size:20px;
+            color:darkred;
+          }
+          .chart-type{
+            color: black;
+            background-color: white;
+          }
+          .col-4{
+             margin: auto;
+          }
+
+        
+    </style>
   </head>
-  <body>
+  <body  class="text-center" >
+    <a id="skippy" class="sr-only sr-only-focusable" href="#content">
+  <div class="container">
+    <span class="skiplink-text">Skip to main content</span>
+  </div>
+</a>
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="masthead mb-auto">
+    <div class="inner">
+      <!-- <h3 class="masthead-brand">Cover</h3> -->
+      <h3 class="masthead-brand">ポモドーロ</h3>
+      <nav class="nav nav-masthead justify-content-center">
+        <!-- <a class="nav-link active" href="#">Home</a> -->
+        <a class="nav-link" href="index.php">ホーム</a>
+        <!-- <a class="nav-link" href="#">Features</a> -->
+        <a class="nav-link active" href="fitbit_index.php">データ</a>
+      </nav>
+    </div>
+  </header>
+
+  <main role="main" class="inner cover">
+    <!-- <h1 class="cover-heading">Cover your page.</h1> -->
+    <h1 class="cover-heading">データ</h1>
+    
+    <!-- <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p> -->
+    <p class="lead"></p>
+    <div class="row chart-type">
+      <div class="col-8">
+    <?php require("show_status.php"); ?>
+        </div>
+      <div class="col-4">
+      <p>1週間のポモドーロ回数の推移です。</p>
+      </div>
+    </div>
+    <P><br></P>
+    <div class="row chart-type">
+      <div class="col-8">
     <p>25分の平均心拍数: <span class="time"></span></p>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <p class="title">25分間の心拍数</p>
             
-        <div id="chart2"></div> 
+        <div id="chart2"></div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js" type="text/javascript"></script>
         <script type="text/javascript">
-            
-            
-            
+             
 //      // 心拍数を取得する
       getHeartrate();
 
@@ -105,8 +224,8 @@
         };
         var options = {
           fullWidth: true,
-          height: 300,
-            width:600,                   //最大値最小値設定
+          height: 200,
+            width:400,                   //最大値最小値設定
           low:50,
           scales: {                          //軸設定
                 yAxes: [{                      //y軸設定
@@ -152,7 +271,7 @@
           if(ave > 65 && varia >10){
             console.log("高ストレス");
           }else if(ave > 65){
-            console.log("長集中");
+            console.log("高集中");
           }else if(varia > 65){
             console.log("集中してない");
           }else{
@@ -161,14 +280,76 @@
         }
 
     </script>
+      </div>
+    </div>
+    <p class="lead">
+      <!-- <a href="#" class="btn btn-lg btn-secondary">Learn more</a> -->
+    </p>
+    <div class="card-deck mb-3 text-center">
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <!-- <h4 class="my-0 font-weight-normal">Free</h4> -->
+        <h4 class="my-0 font-weight-normal">今日の作業時間</h4>
+      </div>
+      <div class="card-body">
+        <!-- <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1> -->
+        <?php todayWorkTime(0); ?>
+      </div>
+    </div>
+      
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <!-- <h4 class="my-0 font-weight-normal">Free</h4> -->
+        <h4 class="my-0 font-weight-normal">今日のポモドーロ回数</h4>
+      </div>
+      <div class="card-body">
+        <!-- <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1> -->
+        <?php todayWorkTime(1); ?>
+      </div>
+    </div>
+  </div>
+    
+      <div class="card-deck mb-3 text-center">
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <!-- <h4 class="my-0 font-weight-normal">Free</h4> -->
+        <h4 class="my-0 font-weight-normal">今週の作業時間</h4>
+      </div>
+      <div class="card-body">
+        <!-- <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1> -->
+        <?php todayWorkTime(2); ?>
+      </div>
+    </div>
+      
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <!-- <h4 class="my-0 font-weight-normal">Free</h4> -->
+        <h4 class="my-0 font-weight-normal">今週のポモドーロ回数</h4>
+      </div>
+      <div class="card-body">
+        <!-- <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1> -->
+        <?php todayWorkTime(3); ?>
+      </div>
+    </div>
+  </div>
+    
+    <div id="comment">
+        <p>----コメント----</p>
+        <p>Quiita</p>
+    </div>
+    
+  </main>
+
+  <footer class="mastfoot mt-auto">
+    <div class="inner">
+      <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    </div>
+  </footer>
+</div>
+
+
   </body>
 </html>
 
-<style type="text/css">
-            .title{margin:30px 0 0;text-align:center;}
-            #chart2{
-              margin:10px auto;
-              width:100%;
-              max-width:600px;
-            }  
-</style>
+
+
